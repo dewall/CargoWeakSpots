e4dab4f72c6a8cc6a61db8a819479a45cfc1355c
dewall <a.dewall@52north.org>
2016-12-16 22:19:46 +0100

    some stuff

---
+++ app/SliderUtils.js
@@ -0,0 +1,12 @@
+define('app/utils/SliderUtils', [
+    "esri/tasks/support/Query"
+], function(Query) {
+
+    function initSlider() {
+
+    }
+
+    return {
+        initSlider: initSlider
+    }
+});
