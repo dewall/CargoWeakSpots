89ae07683ed1bee0c7be1871f4d5ebebbbee3db1
dewall <a.dewall@52north.org>
2016-12-17 16:36:18 +0100

    last mile

--- app/main.js
+++ app/main.js
@@ -20,14 +20,20 @@ require([
     "esri/dijit/LayerList",
     "dojo/dom-construct",
     "dojo/dom-class",
+    "esri/dijit/LayerList",
     "dijit/registry",
     "dojox/form/RangeSlider",
     "dojo/_base/Color",
+
     "dojo/dom",
     "dojo/on",
     "dojo/domReady!"
-], function (Map, FeatureLayer, SimpleLineSymbol, SimpleRenderer, ClassBreaksRenderer, HorizontalSlider, smartMapping, ClassedColorSlider, ColorInfoSlider, FeatureLayerStatistics, InfoTemplate, Popup, PopupTemplate, Extent, SimpleFillSymbol, SimpleLineSymbol,
-        GeometryService, Query,LayerList, domConstruct, domClass, registry, RangeSlider, Color, dom, on) {
+], function(Map, FeatureLayer, SimpleLineSymbol, SimpleRenderer,
+    ClassBreaksRenderer, HorizontalSlider, smartMapping, ClassedColorSlider,
+    ColorInfoSlider, FeatureLayerStatistics, InfoTemplate, Popup,
+    PopupTemplate, Extent, SimpleFillSymbol, SimpleLineSymbol,
+    GeometryService, Query, LayerList, domConstruct, domClass,
+    LayerList, registry, RangeSlider, Color, dom, on) {
 
     var selected = "VSumGes";
     var attributes = {
@@ -66,57 +72,60 @@ require([
     // Carbon storage of trees in Warren Wilson College.
     var symbol = new SimpleLineSymbol();
     symbol.setColor(new Color([150, 150, 150, 0.5]));
-    symbol.setWidth(3);
+    symbol.setWidth(5);
 
-    // var renderer = new SimpleRenderer({
-    //     symbol: sls
-    // });
+    var renderer = new SimpleRenderer({
+        symbol: symbol
+    });
 
     var colorInfoSlider = new ColorInfoSlider({
         colorInfo: {
             stops: [{
-                    color: new Color("#FFFCD4"),
-                    value: 0.0001
-                }, {
-                    color: new Color("#FFFCD4"),
-                    value: 1.0001
-                }]
+                color: new Color("#FFFCD4"),
+                value: 0.0001
+            }, {
+                color: new Color("#FFFCD4"),
+                value: 1.0001
+            }]
         }
     }, "esri-colorinfoslider");
     colorInfoSlider.startup();
 
-
-
-
-    // var renderer = new ClassBreaksRenderer(symbol, "VSumGes");
-    // renderer.addBreak(0, 2000, new SimpleLineSymbol().setColor(new Color([0, 255, 0, 0.5])));
-    // renderer.addBreak(5000, 10000, new SimpleLineSymbol().setColor(new Color([255, 255, 0, 0.5])));
-    // renderer.addBreak(10000, 20000, new SimpleLineSymbol().setColor(new Color([255, 0, 0, 0.5])));
-
     var featureLayer = new FeatureLayer("http://services.arcgis.com/CHWy5Vg5bILt6ufC/arcgis/rest/services/DBhackathon_Grunddaten_V2_extra_Download/FeatureServer/2", {
-        mode: FeatureLayer.MODE_AUTO,
         outFields: "*"
     });
     var featureLayerStatistics = new FeatureLayerStatistics({
         layer: featureLayer,
         visible: true
     });
+    var nullSegmentsFL = new FeatureLayer("http://services.arcgis.com/CHWy5Vg5bILt6ufC/arcgis/rest/services/StreckennetzCargoNull_Werte_Netz/FeatureServer/0");
 
     var popup = new Popup({
         fillSymbol: new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-                new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
-                        new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.25]))
+            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.25]))
     }, domConstruct.create("div"));
 
     domClass.add(popup.domNode, "myTheme");
 
     var infoTemplate = setInfoTemplate(selected);
 
-    // featureLayer.setRenderer(renderer);
+    featureLayer.setRenderer(renderer);
 
+    // var layerList = new LayerList({
+    //     map: map,
+    //     showLegend: true,
+    //     showSubLayers: false,
+    //     showOpacitySlider: true,
+    //     visibility: false,
+    //     layers: [featureLayer]
+    // }, "layerListContainer");
+    // layerList.startup();
+
+    map.addLayer(nullSegmentsFL);
     map.addLayer(featureLayer);
 
-    featureLayer.on("load", function () {
+    featureLayer.on("load", function() {
         map.addLayer(featureLayer);
 
         // featureLayer.setInfoTemplate(new InfoTemplate("${NAME} County, ${STATE_FIPS:FormatFIPSToStateAbbr()}", content));
@@ -127,14 +136,7 @@ require([
         updateSmartMapping();
     });
 
-//    var layerList = new LayerList({
-//        map: map,
-//        showLegend: true,
-//        showSubLayers: false,
-//        showOpacitySlider: true,
-//        layers: [featureLayer]
-//    }, "layerListDom");
-//    layerList.startup();
+
 
     function initSelector() {
         var select = dom.byId("attrib-selector");
@@ -155,7 +157,9 @@ require([
             field: "VSumGes",
             basemap: "dark-gray",
             theme: "high-to-low"
-        }).then(function (colorRenderer) {
+        }).then(function(colorRenderer) {
+            console.log(colorRenderer.renderer);
+
             hackColorRamp(colorRenderer);
 
             featureLayer.setRenderer(colorRenderer.renderer);
@@ -164,7 +168,7 @@ require([
             featureLayerStatistics.getHistogram({
                 field: selected,
                 numBins: 20
-            }).then(function (histogram) {
+            }).then(function(histogram) {
                 colorInfoSlider.set("colorInfo", colorRenderer.renderer.visualVariables[0]);
                 colorInfoSlider.set("minValue", colorRenderer.statistics.min);
                 colorInfoSlider.set("maxValue", colorRenderer.statistics.max);
@@ -173,7 +177,7 @@ require([
                 colorInfoSlider.set("handles", [0, 4]);
                 colorInfoSlider.set("primaryHandle", null);
 
-                colorInfoSlider.on("handle-value-change", function (sliderValueChange) {
+                colorInfoSlider.on("handle-value-change", function(sliderValueChange) {
                     //console.log("handle-value-change", sliderValueChange);
                     featureLayer.renderer.setVisualVariables([sliderValueChange]);
 
@@ -181,14 +185,14 @@ require([
                     featureLayer.redraw();
                 });
 
-                dom.byId("attrib-selector").onchange = function () {
+                dom.byId("attrib-selector").onchange = function() {
                     selected = this.value;
                     smartMapping.createColorRenderer({
                         layer: featureLayer,
                         field: selected,
                         basemap: "dark-gray",
                         theme: "high-to-low"
-                    }).then(function (colorRenderer) {
+                    }).then(function(colorRenderer) {
                         hackColorRamp(colorRenderer);
 
                         setInfoTemplate(selected);
@@ -203,7 +207,7 @@ require([
                         colorInfoSlider.set("handles", [0, 4]);
                         colorInfoSlider.set("primaryHandle", null);
 
-                    }).otherwise(function (error) {
+                    }).otherwise(function(error) {
                         console.log("Error: %o", error);
                         colorInfoSlider.showHistogram = false;
                     });
@@ -220,6 +224,10 @@ require([
 
     function hackColorRamp(colorRenderer) {
         var tmp = colorRenderer.renderer.visualVariables[0];
+
+        // dirty hack
+        colorRenderer.renderer.defaultSymbol.width = 3;
+        colorRenderer.renderer._symbols["-9007199254740991-9007199254740991"].width = 3;
 
         tmp.stops[4].color = {
             a: 1,
